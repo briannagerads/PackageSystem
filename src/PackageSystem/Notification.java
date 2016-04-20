8a347285e70970f9acc64a2fa538b09ca22cd8d4
Brianna Gerads <brianna.gerads@gmail.com>
2016-04-19 22:54:24 -0500

    fixed notification

--- src/PackageSystem/Notification.java
+++ src/PackageSystem/Notification.java
@@ -1,5 +1,74 @@
 package PackageSystem;
 
 public class Notification {
-
+	
+	public DatabaseSupport database;
+	public Package p;
+	public Resident r;
+	
+	
+	public boolean notificationRecieveText(int packageID) {
+		//get package
+		p = database.getPackage(packageID);
+		
+		//get Resident?
+		//r = database.getResident(packageOwner);
+		
+		//get phone number
+		r.phone = database.getPhone(r);
+		
+		//send text
+		
+		
+		return false;
+	}
+	
+	public boolean notificationRecieveEmail(int packageID) {
+		//get package
+		p = database.getPackage(packageID);
+		
+		//get Resident?
+		//r = database.getResident(packageOwner);
+		
+		//get phone number
+		r.phone = database.getEmail(r);
+		
+		//send email
+		
+		
+		return false;
+	}
+	
+	public boolean notificationDeliverText(int packageID) {
+		//get package
+		p = database.getPackage(packageID);
+		
+		//get Resident?
+		//r = database.getResident(packageOwner);
+		
+		//get phone number
+		r.phone = database.getPhone(r);
+		
+		//send text
+		
+		
+		return false;
+	}
+	
+	public boolean notificationDeliverEmail(int packageID) {
+		//get package
+		p = database.getPackage(packageID);
+		
+		//get Resident?
+		//r = database.getResident(packageOwner);
+		
+		//get phone number
+		r.phone = database.getEmail(r);
+		
+		//send email
+		
+		
+		return false;
+	}
+	
 }
